4f23d83a719b72b996da8e6268e77c431837e6f0
fduraibi <fadnix@gmail.com>
2014-12-04 23:21:01 -0500

    Changed main layout to Linear, now rotation can fill the whole layout

--- app/src/main/java/net/fadvisor/verticalseekbar/MainActivity.java
+++ app/src/main/java/net/fadvisor/verticalseekbar/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 
@@ -19,6 +20,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final View rl2 = findViewById(R.id.rl2);
+        final View rl1 = findViewById(R.id.rl1);
 
         rl2.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @SuppressWarnings("deprecation")
@@ -26,14 +28,19 @@ public class MainActivity extends ActionBarActivity {
             public void onGlobalLayout() {
 
                 //int min = Math.min(rl2.getWidth(), rl2.getHeight());
+                int w1 = rl1.getWidth();
+                int h1 =rl1.getHeight();
                 int w = rl2.getWidth();
                 int h =rl2.getHeight();
 
-                rl2.setRotation(270.0f);
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(h, w);
-                params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-                rl2.setLayoutParams(params);
 
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(h, w);
+                //params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+
+                rl2.setLayoutParams(params);
+                rl2.setRotation(270.0f);
+                rl2.setTranslationX((w - h) / 2);
+                rl2.setTranslationY((h - w) / 2);
 
                 if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN)
                     rl2.getViewTreeObserver().removeOnGlobalLayoutListener(this);
